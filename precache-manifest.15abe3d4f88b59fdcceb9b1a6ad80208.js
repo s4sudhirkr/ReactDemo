22dfae20835dca4bae9989e1a410347fce8753e3
Sudhir Kumar Sharma <sudhir.kumar@crmnext.in>
2019-10-18 13:44:39 +0530

    Updates

---
+++ precache-manifest.15abe3d4f88b59fdcceb9b1a6ad80208.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1037a25c8eba715d183bcd831f0c525",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "e3a26b528719c46e08b0",
+    "url": "/ReactDemo/static/css/2.8c740ac1.chunk.css"
+  },
+  {
+    "revision": "d1511ef74a869344c27a",
+    "url": "/ReactDemo/static/css/main.1740a6f9.chunk.css"
+  },
+  {
+    "revision": "e3a26b528719c46e08b0",
+    "url": "/ReactDemo/static/js/2.5cc601eb.chunk.js"
+  },
+  {
+    "revision": "d1511ef74a869344c27a",
+    "url": "/ReactDemo/static/js/main.28615d80.chunk.js"
+  },
+  {
+    "revision": "71921289e571084cbd5f",
+    "url": "/ReactDemo/static/js/runtime~main.201603d5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactDemo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactDemo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactDemo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactDemo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactDemo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
